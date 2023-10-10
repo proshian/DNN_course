014b23c72ef5b92501992a4a7a46c4f2b1e6808a
Garri Proshian <proshian.itmo@gmail.com>
2023-10-10 21:21:25 +0300

    test_fc.py is a proper unittest now

--- numpy_nn/test/test_fc.py
+++ numpy_nn/test/test_fc.py
@@ -5,7 +5,7 @@ import unittest
 import torch
 import numpy as np
 
-from test_layer import test_module_randomly
+from test_layer import TestLayer
 
 from numpy_nn.modules.np_nn import FullyConnectedLayer
 
@@ -13,35 +13,36 @@ from numpy_nn.modules.np_nn import FullyConnectedLayer
 np_seed = 0
 np.random.seed(np_seed)
 
-class TestDataset(unittest.TestCase):
+class TestFullyConnectedLayer(TestLayer):
 
     def setUp(self) -> None:        
         pass
 
-    def test_fc_(self):
+    def test_fc(self):
         """
         FullyConnectedLayer test
         """
         n_input_neurons = 6
         n_output_neurons = 3
         n_samples = 5
+        n_iters = 3
 
-        my_fc_params = torch_fc_params = [n_input_neurons, n_output_neurons]
+        my_fc_params = torch_fc_params = (n_input_neurons, n_output_neurons)
+        input_shape = (n_samples, n_input_neurons)
+        output_shape = (n_samples, n_output_neurons)
 
-        test_module_randomly(FullyConnectedLayer(*my_fc_params),
-                             torch.nn.Linear(*torch_fc_params),
-                             input_shape=[n_samples, n_input_neurons],
-                             output_shape=[n_samples, n_output_neurons],
-                             random_sampler=np.random.rand)
+        for sampler in (np.random.rand, np.random.randn):
+            for _ in range(n_iters):
+                input_np = sampler(*input_shape).astype(np.float32)
+                dJ_dout = sampler(*output_shape)
 
-    
-
-        test_module_randomly(FullyConnectedLayer(*my_fc_params),
-                             torch.nn.Linear(*torch_fc_params),
-                             input_shape=[n_samples, n_input_neurons],
-                             output_shape=[n_samples, n_output_neurons],
-                             atol=1e-6,
-                             random_sampler=np.random.randn)
+                with self.subTest(input_np = input_np, dJ_dout = dJ_dout, sampler = sampler):
+                    self._test_module(
+                        my_module = FullyConnectedLayer(*my_fc_params),
+                        torch_module = torch.nn.Linear(*torch_fc_params),
+                        input_np = input_np,
+                        dJ_dout = dJ_dout,
+                        atol=1e-6)
         
 
 if __name__ == "__main__":
